f0c710cf99717f6a6d6503beccdbd8b30babc419
waldo1234567 <waldowalerian@gmail.com>
2025-09-11 01:27:43 +0800

    adding auth 0

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain="https://dev-i8y8tcafjtnstqmz.us.auth0.com/"
+      clientId="1AaF03iUw4Hz84aWuWNHW54FHxDMcs1l"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: "https://api.task_management.com" 
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
